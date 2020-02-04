bab536bf3d7e9862b1e0171e4140b1039a588d67
kjacasio <59855973+kjacasio@users.noreply.github.com>
2020-02-04 10:14:47 -1000

    parts 1-4 Feb 4

--- SwiftPlaygroundTwo.playground/Contents.swift
+++ SwiftPlaygroundTwo.playground/Contents.swift
@@ -1,3 +1,25 @@
 import UIKit
 
 var str = "Hello, playground"
+
+var goodGrade = "I've been all my work"
+
+if goodGrade == "I've been doing all my work"
+{
+    print(" I should be getting an A")
+}
+else
+{
+    print("I need to work harder in this class")
+}
+
+var gradeA = true
+    
+if gradeA == true
+{
+    print("I really like this class")
+}
+else
+{
+    print("I'm not sure about coding")
+}
